f1ae7593f6372a2cabb3a04553aafa00360e1e0c
Raymond Hernandez <rhernandez028@west-mec.org>
2019-03-08 18:13:52 -0700

    Created Events
    Created mouse events that selects multiple cells in the table

--- jpf_hanjie.js
+++ jpf_hanjie.js
@@ -58,19 +58,87 @@
 //run the init funtion when the papge loads
 window.onload = init;
 
+var puzzleCells;
+var cellBackground;
+
 function init() {
       //insert the title for the first puzzle
       document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
 
       // insert the HTML code for the first puzzle table
-      document.getElementById("puzzle").innerHTML = drawPuzzle(puzzleHint, puzzleRating, puzzle1);
+      document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
+
+      // add event handlers for the puzzle buttons
+      var puzzleButtons = document.getElementsByClassName("puzzles");
+      for (var i = 0; i < puzzleButtons.length; i++) {
+            puzzleButtons[i].onclick = swapPuzzle;
+      }
+
+      setupPuzzle();
 }
 
+//add an event listener for the mouseup event
+document.addEventListener("mouseup", endBackground);
 
+function swapPuzzle (e) {
+      // retrieves the ID of the clicked button
+      var puzzleID = e.target.id;
 
+      //retrieved the value of the clicked button
+      var puzzleTitle = e.target.value;
+      document.getElementById("puzzleTitle").innerHTML = puzzleTitle;
 
+      //display the puzzle based on the value of the puzzle ID variable
+      switch (puzzleID) {
+            case "puzzle1":
+                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
+                  break;
+            case "puzzle2":
+                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle2Hint, puzzle2Rating, puzzle2);
+                  break;
+            case "puzzle3":
+                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle3Hint, puzzle3Rating, puzzle3);
+                  break;
+      }
 
-         
+      setupPuzzle();
+}
+
+function setupPuzzle() {
+      // match all of the data cells in the puzzle
+      puzzleCells = document.querySelectorAll("table#hanjieGrid td");
+
+      //set the initial color of each cell to gold
+      for (var i = 0; i < puzzleCells.length; i++) {
+            puzzleCells[i].style.backgroundColor = "rgb(233, 207, 29)";
+            
+            // set cell background color in response to the mousedown event
+            puzzleCells[i].onmousedown = setBackground; 
+      }
+}
+
+function setBackground(e) {
+      cellBackground = "rgb(101, 101, 101)";
+      e.target.style.backgroundColor = cellBackground;
+
+      //create an event lister for every puzzle cell
+      for (var i = 0; i < puzzleCells.length; i++ ) {
+            puzzleCells[i].addEventListener("mouseenter", extendBackground);
+      }
+}
+
+//creat a function to extend the backgrund and set a new background color
+function extendBackground(e) {
+      e.target.style.backgroundColor = cellBackground;
+}
+
+function endBackground() {
+      //remove the event listner for evry puzzle cell
+      for (var i = 0; i < puzzleCells.length; i++ ) {
+            puzzleCells[i].removeEventListener("mouseenter", extendBackground);
+      }
+}
+
 /* ================================================================= */
 
 function drawPuzzle(hint, rating, puzzle) {
